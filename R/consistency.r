5f96ecdf09a028a35d723e596df07983b7af0f13
giglhorn <sam-sed@web.de>
2018-01-11 21:39:07 +0100

    new plots, improved report and R files

--- R/consistency.r
+++ R/consistency.r
@@ -1,5 +1,5 @@
 setwd("/home/s/Dokumente/R&D/IoTTrace/data/")
-data <- read.csv("taxi0228.csv")
+data <- read.csv("taxi-medium-big.csv")
 names(data) <- c("id","taxiid","longitude","latitude","speed","angle","datetime","status","extendedstatus","reversed")
 dataorig <- data
 datacount <- nrow(data)
@@ -39,12 +39,6 @@ ggmap(map) +
 #speed values before cutting off certain coordinates
 summary(data$speed)
 
-#density of speed before the coordinate cut-off
-plot(density(data$speed), xlab="Speed", ylab="Density", main="Speed density")
-dataw0 <- subset(data,speed>0 && speed<150)
-hist(dataw0$speed, xlab="Speed", ylab="Density", main="Adapted speed distribution without zero values")
-plot(density(dataw0$speed), xlab="Speed", ylab="Density", main="")
-
 # speed filtering
 boxplot(data$speed)
 plot(density(data$speed), xlab="Speed", ylab="Density", main="")
@@ -53,7 +47,7 @@ summary(data$speed)
 upq <- quantile(data$speed, 0.75)
 # limit is 3x the interquartile range
 # limit <- upq + 3*(upq-quantile(data$speed, 0.25))
- limit <- 100
+limit <- 100
 data <- subset(data, speed < limit)
 summary(data$speed)
 dropped <- datacount - nrow(data)
@@ -61,20 +55,42 @@ dropped <- datacount - nrow(data)
 dropped
 summary(data$speed)
 
+#density of speed before the coordinate cut-off
+plot(density(data$speed), xlab="Speed", ylab="Density", main="Speed density")
+dataw0 <- subset(data, speed>0)
+hist(dataw0$speed, xlab="Speed", ylab="Density", main="Adapted speed distribution before coordinate cut-off")
+mean_before <- mean(dataw0$speed)
+median_before <- median(data$speed)
+u80_before <- nrow(subset(data, speed>=80))
+plot(density(dataw0$speed), xlab="Speed", ylab="Density", main="Adapted speed density before coordinate cut-off") +
+   abline(v=mean_before)
+
 # coordinate filtering
 plot(data$longitude, data$latitude, main="unfiltered Map", xlab="Longitude", ylab="Latitude")
+datastat <- subset(data, longitude>=x_start)
 data <- subset(data, longitude>=x_start)
+datastat <- subset(data, longitude<=x_end)
 data <- subset(data, longitude<=x_end)
+datastat <- subset(data, latitude>=y_start)
 data <- subset(data, latitude>=y_start)
+datastat <- subset(data, latitude<=y_end)
 data <- subset(data, latitude<=y_end)
 plot(data$longitude, data$latitude, main="filtered Map", xlab="Longitude", ylab="Latitude", pch=".")
 dropped <- datacount - nrow(data)
 #number of dropped observations
 dropped
+datastat <- subset(data, speed > median_before)
+nrow(datastat)/datacount
 summary(data$speed)
 
-dataw0 <- subset(data,speed>0 && speed<150)
-plot(density(dataw0$speed), xlab="Speed", ylab="Density", main="")
+#density of speed after the coordinate cut-off
+dataw0_2 <- subset(data, speed > 0)
+hist(dataw0_2$speed, xlab="Speed", ylab="Density", main="Adapted speed distribution after coordinate cut-off")
+mean_after <- mean(dataw0_2$speed)
+median_after <- median(data$speed)
+u80_after <- nrow(subset(data, speed>=80))
+plot(density(dataw0_2$speed), xlab="Speed", ylab="Density", main="Adapted speed density after coordinate cut-off") +
+  abline(v=mean_after)
 
 hist(data$status, xlab="status", ylab="frequency", main="Taxi status", xlim = c(-0.5,3.5), breaks=c(-0.5,0.5,1.5,2.5,3.5))
 hist(data$extendedstatus, xlab="extended status", ylab="frequency", main="Extended taxi status", xlim = c(-0.5,3.5), breaks=c(-0.5,0.5,1.5,2.5,3.5))
--- R/distances.r
+++ R/distances.r
@@ -10,4 +10,7 @@ data$timeToNextTrip <- data$timeToNextTrip / 60
 data <- subset(data, data$timeToNextTrip > 0)
 data <- subset(data, data$timeToNextTrip < 100)
 data <- subset(data, data$distance > 0)
-hist(data$timeToNextTrip, xlab = "time to the next trip in minutes", ylab = "Frequency", main = "Time gap distribution")
+mean <- mean(data$timeToNextTrip)
+hist(data$timeToNextTrip, xlab = "Time to the next trip in minutes", ylab = "Frequency", main = "Time gap distribution")
+plot(density(data$timeToNextTrip), xlab="time to the next trip", ylab="Density", main="Time gap density") +
+  abline(v = mean)
